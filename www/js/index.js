51874829a3fb316c6bf398f346b7f2f60e45acdb
smapps <info@smapps.nl>
2016-01-20 15:02:27 +0100

    Veel nieuws

--- www/js/index.js
+++ www/js/index.js
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- */
+ 
 
     function onDeviceReady()
 	{
@@ -40,3 +40,4 @@
 	  }
 	}
 	
+*/
